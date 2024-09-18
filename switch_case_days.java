42f1ff4d7f73e37fda3713bbb266829e7d99020a
bader <bader5uk@hotmail.com>
2024-09-18 11:46:36 +0400

    edited for user input

--- switch_case_days.java
+++ switch_case_days.java
@@ -1,32 +1,35 @@
+import java.util.Scanner;
+
 public class switch_case_days {
     public static void main(String[] args) {
-       String dayname = "";
-        for (int day =1; day<=7; day++) {
-            switch (day) {
-                case 1:
-                    dayname = "Sunday";
-                    break;
-                case 2:
-                    dayname = "Monday";
-                    break;
-                case 3:
-                    dayname = "Tuesday";
-                    break;
-                case 4:
-                    dayname = "Wednesday";
-                    break;
-                case 5:
-                    dayname = "Thursday";
-                    break;
-                case 6:
-                    dayname = "Friday";
-                    break;
-                case 7:
-                    dayname = "Saturday";
-                    break;
-            }
-            System.out.print(dayname + ", ");
+        String dayname = "";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a day number:  ");
+        int userinp = in.nextInt();
+        switch (userinp) {
+            case 1:
+                dayname = "Sunday";
+                break;
+            case 2:
+                dayname = "Monday";
+                break;
+            case 3:
+                dayname = "Tuesday";
+                break;
+            case 4:
+                dayname = "Wednesday";
+                break;
+            case 5:
+                dayname = "Thursday";
+                break;
+            case 6:
+                dayname = "Friday";
+                break;
+            case 7:
+                dayname = "Saturday";
+                break;
         }
+        System.out.print(dayname);
     }
 }
 
